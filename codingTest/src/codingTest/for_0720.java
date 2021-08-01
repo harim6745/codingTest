5cdaf8474cb8e435831ee42fa64f701e58241c14
harim <harim6745@naver.com>
2021-08-01 15:37:27 +0900

    0723

--- codingTest/src/codingTest/for_0720.java
+++ codingTest/src/codingTest/for_0720.java
@@ -6,16 +6,45 @@ public class for_0720 {
 
 	public static void main(String[] args) {
 		
+//		//2741
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int n;
+//		
+//		System.out.println("n : ");		
+//		n = sc.nextInt();
+//		
+//		for ( int i = 1; i <= n; i++) {
+//			System.out.println(i);
+//			
+//		}
+//		
+//		//2742
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int n;
+//		
+//		System.out.println("n : ");		
+//		n = sc.nextInt();
+//		
+//		for ( int i = n; i >= 1; i--) {
+//			System.out.println(i);
+//			
+//		}
+		
+		//11021
 		Scanner sc = new Scanner(System.in);
 		
-		int n;
+		int A, B, T;
+		T = sc.nextInt();
 		
-		System.out.println("n : ");		
-		n = sc.nextInt();
-		
-		for ( int i = 1; i <= n; i++) {
-			System.out.println(i);
+		for (int i = 1; i <= T; i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			
+			System.out.println("Case #" + i + ": " + (A+B));
 		}
+		
 
 	}
 }
